6cb7a671e59f22f73f88580d6a5fab55b9f41cb6
narugo1992 <narugo992@gmail.com>
2023-11-12 20:12:33 +0800

    dev(narugo): init project

---
+++ sdeval/config/meta.py
@@ -0,0 +1,19 @@
+"""
+Overview:
+    Meta information for sdeval package.
+"""
+
+#: Title of this project (should be `sdeval`).
+__TITLE__ = 'sdeval'
+
+#: Version of this project.
+__VERSION__ = '0.0.1'
+
+#: Short description of the project, will be included in ``setup.py``.
+__DESCRIPTION__ = 'Evaluation for stable diffusion model training'
+
+#: Author of this project.
+__AUTHOR__ = 'narugo1992'
+
+#: Email of the authors'.
+__AUTHOR_EMAIL__ = 'narugo992@gmail.com'
